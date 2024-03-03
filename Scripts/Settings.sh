cfcf91068e81a1abcf34267b362dfbcf868e75bd
Teacher-c <58407665+Teacher-c@users.noreply.github.com>
2024-03-03 23:26:14 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
+#SSR-Plus
+#修复 shadowsocksr-libev libopenssl-legacy 依赖问题。
+#因已回退插件库，不需要修复。
+#sed -i 's/ +libopenssl-legacy//g' feeds/smpackage/shadowsocksr-libev/Makefile
+
+#passawall
+#编译新版Sing-box和hysteria，需golang版本1.20或者以上版本
+#lede仓库已是1.20以上版本
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+
+#不知道哪里来的阿里云盘？
+#rm -rf package/feeds/packages/aliyundrive-webdav
+
 #删除冲突插件
-rm -rf $(find ./feeds/luci/ -type d -regex ".*\(argon\|design\|openclash\).*")
+rm -rf $(find ./feeds/luci/ -type d -regex ".*\(argon\|design\).*")
+rm -rf $(find ./feeds/packages/ -type d -regex ".*\(argon\|design\).*")
+rm -rf $(find ./feeds/smpackage/ -type d -regex ".*\(argon\|design\|openclash\).*")
+
+#small-package推荐删除防止与lede库冲突
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
+
 #修改默认主题
 sed -i "s/luci-theme-bootstrap/luci-theme-$OWRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 #修改默认IP地址
