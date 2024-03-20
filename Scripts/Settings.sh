1f50ff6d4a49e8e8947d62c6957df30316cde3e2
Teacher-c <58407665+Teacher-c@users.noreply.github.com>
2024-03-21 04:17:04 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -32,7 +32,7 @@ if [[ "$OWRT_TARGET" == "Redmi-AX6" && "$OWRT_URL" == *"NSS"* ]]; then
   rm -rf ./luci_tmp
 
   #删除作者库自定义插件
-  rm -rf $(find ./package/new/ -type d -regex ".*\(openclash\|argon\|vlmcsd\|cpufreq\|coremark\).*")
+  rm -rf $(find ./package/new/ -type d -regex ".*\(openclash\|argon\|vlmcsd\|cpufreq\|coremark\|v2ray\).*")
 
   #删除作者config文件对应配置
   sed -i '/cpufreq/d' ./AX6.config
@@ -42,7 +42,7 @@ if [[ "$OWRT_TARGET" == "Redmi-AX6" && "$OWRT_URL" == *"NSS"* ]]; then
   sed -i '/theme-bootstrap/d' ./AX6.config
   sed -i '/ddns/d' ./AX6.config
   sed -i '/coremark/d' ./AX6.config
-  #sed -i '/v2ray/d' ./AX6.config
+  sed -i '/v2ray/d' ./AX6.config
   sed -i '/theme-argon/d' ./AX6.config
   sed -i '/mosdns/d' ./AX6.config
   
