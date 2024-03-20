57932013a34a5306e15cf44e4155ee496addbcdd
Teacher-c <58407665+Teacher-c@users.noreply.github.com>
2024-03-21 04:00:57 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -32,7 +32,7 @@ if [[ "$OWRT_TARGET" == "Redmi-AX6" && "$OWRT_URL" == *"NSS"* ]]; then
   rm -rf ./luci_tmp
 
   #删除作者库自定义插件
-  rm -rf $(find ./package/ -type d -regex ".*\(new\).*")
+  #rm -rf $(find ./package/ -type d -regex ".*\(new\).*")
 
   #删除作者config文件对应配置
   sed -i '/cpufreq/d' ./AX6.config
