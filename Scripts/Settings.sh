3cda7024e3b34c7d19a66c3147f8cc85fc911bcd
Teacher-c <58407665+Teacher-c@users.noreply.github.com>
2024-03-25 06:05:09 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -65,6 +65,8 @@ if [[ "$OWRT_URL" == "https://github.com/qosmio/openwrt-ipq.git" ]]; then
 
   cp -rf $GITHUB_WORKSPACE/general/AX6/ipq8071-ax6-stock.dts target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/
 
+  rm -rf package/network/services/dnsmasq
+
   #添加支持firewall4的turboacc加速,有nss编译失败？
   #curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
