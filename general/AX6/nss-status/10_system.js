48cf59d47c66084bb8cec778b11dbc2ad0d632d2
Teacher-c <58407665+Teacher-c@users.noreply.github.com>
2024-05-06 19:44:49 +0800

    Update 10_system.js

--- general/AX6/nss-status/10_system.js
+++ general/AX6/nss-status/10_system.js
@@ -106,7 +106,7 @@ return baseclass.extend({
 				systeminfo.load[2] / 65535.0
 			) : null,
 			_('CPU usage (%)'),    cpuusage.cpuusage,
-			//_('NSS Load'),         (L.isObject(nssinfo) ? 'Avg: %s Max: %s'.format(nssinfo.avg, nssinfo.max) : null)
+			//_('NSS Load'),         (L.isObject(nssinfo) ? 'Min: %s Avg: %s Max: %s'.format(nssinfo.min nssinfo.avg, nssinfo.max) : null)
 			_('NSS'),              (L.isObject(nssinfo) ? '%s'.format(nssinfo.avg) : null)
 		];
 
