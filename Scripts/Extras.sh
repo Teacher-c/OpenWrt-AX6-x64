c06db298f7861d641356b0d92cead8afe159c2a6
Teacher-c <58407665+Teacher-c@users.noreply.github.com>
2024-05-19 15:26:22 +0800

    Update Extras.sh

--- Scripts/Extras.sh
+++ Scripts/Extras.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+#设置kennel和root分区大小
+echo "CONFIG_TARGET_KERNEL_PARTSIZE=64" >> .config
+echo "CONFIG_TARGET_ROOTFS_PARTSIZE=2048" >> .config
+
+#不打包EXT4FS固件
+echo "CONFIG_TARGET_ROOTFS_EXT4FS=n" >> .config
+
+#增加组件一些基础插件
+echo "CONFIG_PACKAGE_luci-compat=y" >> .config
+echo "CONFIG_PACKAGE_luci-lib-ipkg=y" >> .config
+echo "CONFIG_PACKAGE_ipv6helper=y" >> .config
+echo "CONFIG_PACKAGE_ip6tables-extra=y" >> .config
+echo "CONFIG_PACKAGE_ip6tables-mod-nat=y" >> .config
+echo "CONFIG_PACKAGE_unzip=y" >> .config
+echo "CONFIG_PACKAGE_coreutils=y" >> .config
+echo "CONFIG_PACKAGE_coreutils-sort=y" >> .config
+echo "CONFIG_PACKAGE_curl=y" >> .config
+echo "CONFIG_PACKAGE_htop=y" >> .config
+echo "CONFIG_PACKAGE_bash=y" >> .config
+echo "CONFIG_PACKAGE_autocore=y" >> .config
+
 #增加主题
 echo "CONFIG_PACKAGE_luci-theme-$OWRT_THEME=y" >> .config
 #echo "CONFIG_PACKAGE_luci-app-$OWRT_THEME-config=y" >> .config
