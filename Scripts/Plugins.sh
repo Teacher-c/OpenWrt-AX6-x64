a4485bbcfc03af7beeaf865197e4222e2faa6743
Teacher-c <58407665+Teacher-c@users.noreply.github.com>
2024-05-27 18:51:25 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #自定义单独下载仓库插件函数
 function git_sparse_package(){
-    # 参数1是分支名,参数2是库地址。所有文件下载到当前路径./。
+    # 参数1是分支名,参数2是库地址。所有文件下载到当前路径./Add_package。
     # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
     trap 'rm -rf "$tmpdir"' EXIT
     branch="$1" curl="$2" && shift 2
     rootdir="$PWD"
-    localdir=./
+    localdir=./Add_package
     [ -d "$localdir" ] || mkdir -p "$localdir"
     tmpdir="$(mktemp -d)" || exit 1
     git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
