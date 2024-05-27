4b5a22c0448b2635252e803b7847c3eaf04d6f2f
Teacher-c <58407665+Teacher-c@users.noreply.github.com>
2024-05-27 13:56:37 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -33,11 +33,13 @@ fi
 
 if [[ "$OWRT_URL" == "https://github.com/TerryLip/AX6NSS.git" ]]; then
 
-  #添加nss占用信息显示
+  #添加CPU,温度，以及nss占用信息显示
   rm -rf feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
   rm -rf feeds/luci/modules/luci-mod-status/root/usr/share/rpcd/acl.d/luci-mod-status.json
-  cp -rf $GITHUB_WORKSPACE/general/AX6/nss-status/openwrt/10_system.js feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/
-  cp -rf $GITHUB_WORKSPACE/general/AX6/nss-status/openwrt/luci-mod-status.json feeds/luci/modules/luci-mod-status/root/usr/share/rpcd/acl.d/
+  rm -rf feeds/luci/modules/luci-base/root/usr/share/rpcd/ucode/luci
+  cp -rf $GITHUB_WORKSPACE/general/AX6/nss-status/immortal/10_system.js feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/
+  cp -rf $GITHUB_WORKSPACE/general/AX6/nss-status/immortal/luci-mod-status.json feeds/luci/modules/luci-mod-status/root/usr/share/rpcd/acl.d/
+  cp -rf $GITHUB_WORKSPACE/general/AX6/nss-status/luci_file_from_immortal/luci feeds/luci/modules/luci-base/root/usr/share/rpcd/ucode/
   #echo 'skip'
 
   #移除作者插件
